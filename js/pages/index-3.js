8911eadd0ab1683196967cbd814c495819db8c9c
tondewellington-prog <tondewellington@gmail.com>
2026-09-04 14:03:30 +0200

    Fix formatting of service worker registration code

--- js/pages/index-3.js
+++ js/pages/index-3.js
@@ -1,16 +1,15 @@
 if ('serviceWorker' in navigator) {
-        window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/sw.js', { 
-                updateViaCache: 'none'
-            }).then(registration => {
-                console.log('Service Worker registered with scope:', registration.scope);
-                
-                setInterval(() => {
-                    registration.update();
-                    console.log('Checking for service worker updates...');
-                }, 60000);
-            }).catch(err => {
-                console.log('Service Worker registration failed:', err);
-            });
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js', { 
+            updateViaCache: 'none'
+        }).then(registration => {
+            console.log('Service Worker registered with scope:', registration.scope);
+            setInterval(() => {
+                registration.update();
+                console.log('Checking for service worker updates...');
+            }, 60000);
+        }).catch(err => {
+            console.log('Service Worker registration failed:', err);
         });
-    }
+    });
+}
